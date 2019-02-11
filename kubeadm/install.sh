8a46762cc4b72924b71d9cc7380eb99c11ed6ecd
terry <jstel@126.COM>
2019-02-11 05:57:13 -0500

    试验安装cri

--- kubeadm/install.sh
+++ kubeadm/install.sh
@@ -15,10 +15,10 @@ sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
 
 yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
 
-systemctl enable --now kubelet && systemctl start kubelet
+systemctl enable --now kubelet
 
-cat <<EOF >  /etc/sysctl.d/k8s.conf
-net.bridge.bridge-nf-call-ip6tables = 1
-net.bridge.bridge-nf-call-iptables = 1
-EOF
-sysctl --system
+
+sleep 10s
+
+systemctl daemon-reload
+systemctl restart kubelet
