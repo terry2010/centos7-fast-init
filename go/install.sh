74139c30b121545bcfcabd73327b86d381b8838a
terry2010 <jstel@126.com>
2019-04-09 10:52:48 +0800

    update  go version to 11.5

--- go/install.sh
+++ go/install.sh
@@ -1,4 +1,4 @@
-wget https://dl.google.com/go/go1.11.4.linux-amd64.tar.gz
+wget https://dl.google.com/go/go1.11.5.linux-amd64.tar.gz
 tar -C /usr/local -xzf go1.11.4.linux-amd64.tar.gz
 echo '
 export GOROOT=/usr/local/go 
