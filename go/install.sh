f446c02c5a9ad5c483e005e500dce89c59273a55
terry2010 <jstel@126.com>
2019-04-09 10:53:24 +0800

    update go version to 1.11.5

--- go/install.sh
+++ go/install.sh
@@ -1,5 +1,5 @@
 wget https://dl.google.com/go/go1.11.5.linux-amd64.tar.gz
-tar -C /usr/local -xzf go1.11.4.linux-amd64.tar.gz
+tar -C /usr/local -xzf go1.11.5.linux-amd64.tar.gz
 echo '
 export GOROOT=/usr/local/go 
 export GOBIN=$GOROOT/bin 
