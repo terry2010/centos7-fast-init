1f1fa0ada18d7e1cdf64f9682bd844c2848de2fe
terry2010 <jstel@126.com>
2019-05-11 17:38:40 +0800

    Update vps.sh

--- vps/vps.sh
+++ vps/vps.sh
@@ -24,7 +24,7 @@ wget -N --no-check-certificate https://github.com/91yun/serverspeeder/raw/master
 mv ./data1 /
 #libev版本需要内核版本>3.7 ,否则会出 ERROR: failed to set TCP_FASTOPEN_CONNECT
 #docker run -d -p 9000:9000 -p 9000:9000/udp --name ss-libev -v /data1/conf/ss:/etc/shadowsocks-libev teddysun/shadowsocks-libev
-docker run -d -p 9999:9999 -p 9999:9999/udp --name ss-go -v /data1/conf/ss:/etc/shadowsocks-go teddysun/shadowsocks-go
+docker run -d -p 9999:9999 -p 9999:9999/udp --restart=allways --name ss-go -v /data1/conf/ss:/etc/shadowsocks-go teddysun/shadowsocks-go
 
 
 
