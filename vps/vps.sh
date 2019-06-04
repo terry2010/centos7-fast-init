efc861ee72bcd616cdc34cdc230f91b2c5dd0838
terry2010 <jstel@126.com>
2019-06-04 14:31:17 +0800

    Update vps.sh

--- vps/vps.sh
+++ vps/vps.sh
@@ -8,7 +8,8 @@ yum update -y
 wget -N --no-check-certificate  https://github.com/terry2010/centos7-fast-init/raw/master/docker/install.sh && bash install.sh
  
 wget -N --no-check-certificate https://github.com/91yun/serverspeeder/raw/master/serverspeeder.sh && bash -c "yes '1'|sh serverspeeder.sh"
-
+#centos 7.2 强安
+#wget -N --no-check-certificate https://github.com/91yun/serverspeeder/raw/master/serverspeeder-v.sh && bash serverspeeder-v.sh CentOS 7.2 3.10.0-327.el7.x86_64 x64 3.11.20.5 serverspeeder_72327
  
  
  
